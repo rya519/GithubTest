67bd3cdd1b49217b4384c7812c22d2cb95eef7d1
rya519x <amirr@go.stockton.edu>
2021-03-07 23:07:29 -0500

    Initial Commit

---
+++ Shared/GithubTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubTestApp.swift
+//  Shared
+//
+//  Created by Rachel Amir on 3/7/21.
+//
+
+import SwiftUI
+
+@main
+struct GithubTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
